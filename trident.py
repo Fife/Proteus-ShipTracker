7fb18bfb405d87e800913511fc52b8c35fdbe6f6
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2021-08-08 18:40:16 -0600

    Update trident.py

--- trident.py
+++ trident.py
@@ -1,5 +1,6 @@
 from scipy import spatial
 import numpy as np
+import pandas as pd
 
 #Generalized formula for finding cartesian coords given lat and long
 def cartesian(latitude, longitude):
@@ -33,3 +34,32 @@ def findNearestPort(lat, long, tree, dataFrame):
         return 0
     else:
         return dataFrame.port[index]
+    
+def generateGuesses(dF, vessel, start_p, numGuesses):
+    dFGlobal = dF
+    guessFrame = pd.DataFrame()
+    dF = dF[dF['vessel']==vessel]
+    tempList=[]
+    tempList.append(vessel)
+    for x in range(numGuesses):
+        lastStart = start_p
+        # If no suitable end port is found,pull data from global model
+        if (dF[(dF['vessel']== vessel) & (dF['begin_port_id']==start_p)]['guess'].empty):
+            temp = dFGlobal
+
+            temp = temp.drop_duplicates(subset = 'end_port_id')
+            temp = dFGlobal[dFGlobal['begin_port_id'] == start_p]
+            guess = temp[temp['GlobalWeight'] ==temp['GlobalWeight'].max()]
+            guess_p = guess['end_port_id'].iloc[0]
+            start_p = guess_p
+        #Else, pull data from suitable historical model
+        else:
+            guess = dF[(dF['vessel']== vessel) & (dF['begin_port_id']==start_p)]['guess'].max()
+            guess_p = dF[dF['guess'] == guess].drop_duplicates()['end_port_id'].iloc[0]
+            start_p = guess_p
+
+        guessFrame = guessFrame.append(pd.DataFrame({'vessel' : [vessel] ,
+                                'begin_port_id' : [lastStart],
+                                'end_port_id'  : [guess_p],
+                                'voyage' : [x+1] }))
+    return guessFrame
